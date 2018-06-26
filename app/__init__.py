0ffb21e01d39c3d6fc8de23e9fe4ced85a55b048
kenneth-rosario <kenneth.rosario7@upr.edu>
2018-06-25 23:15:11 -0400

    added landing page to server

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_file
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/app/static/images')
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # from models import Result ; Example code for future reference
 
+
 @app.route('/')
-def hello():
-    return "Hello World!"
+def home():
+    return render_template("landing-page.html")
+
+
+@app.route('/images/<string:image>')
+def return_image(image):
+    filename = os.path.join(app.instance_path, 'static', 'images', image).replace("instance", "app")
+    return send_file(filename , mimetype="image/gif")
 
